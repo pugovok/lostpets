57127f4e803b5298b4bff1dc3146adc400931c35
Maxim Kuptsov <maximkuptsov@gmail.com>
2014-12-12 13:22:58 +0300

    fix a serious bug google API. small, but very necessary, change. Sorry :(

--- frontend/web/app/views/map.js
+++ frontend/web/app/views/map.js
@@ -58,8 +58,8 @@ define([
 		boundsChanged: function() {
 			var bounds = window.gmap.getBounds();
 			window.app.Filter.set({
-				topleft:  [bounds.wa.j, bounds.Fa.j],
-				botright: [bounds.wa.k, bounds.Fa.k]
+				topleft:  [bounds.getSouthWest().D, bounds.getNorthEast().k],
+				botright: [bounds.getNorthEast().k, bounds.getSouthWest().k]
 			});
 
 		}
